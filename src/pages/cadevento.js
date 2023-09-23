8e0798a7e735550abf75e4e1db8e267276b65474
VictorRaphaelSF <ferreir.dsf@gmail.com>
2023-09-23 02:12:48 -0300

    Patch 1.3.2 (Fixed CSS in Cadevento.js)

--- src/pages/cadevento.js
+++ src/pages/cadevento.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Pressable, Image, Platform, Dimensions, TextInput, ScrollView, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, Pressable, Image, Platform, Dimensions, TextInput, ImageBackground } from 'react-native';
 
 import * as Animatable from 'react-native-animatable';
 import * as ImagePicker from 'expo-image-picker';
 import { useNavigation } from '@react-navigation/native';
 
-export default function Cadevento() {
-  const [setnmevento, setNmEvento] = useState('');
+export default function cadevento({route}) {
+
+  const [nmuevento, setNmevento] = useState('');
   const [descrição, setDescrição] = useState('');
   const [erro, setErro] = useState('');
   const [image, setImage] = useState(null);
-  const [cep, setEndereco] = useState('')
-  const [numero, setCep] = useState('')
   const navigation = useNavigation();
-
+  
   const handleImagePicker = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       aspect: [4, 4],
@@ -25,11 +24,10 @@ export default function Cadevento() {
       setImage(result.assets[0].uri);
     }
   };
-
+  
   const backbutton = () => {
     navigation.goBack();
   };
-
   
   const renderCaracteresRestantes = () => {
     const caracteresRestantes = 255 - descrição.length;
@@ -41,42 +39,39 @@ export default function Cadevento() {
     );
   };
   
-  const eventData = {
-    nmEvent : setnmevento,
-    descricao : descrição,
-    cep: cep,
-    numero: numero,
-    uri : "imagem.png"
-  }
+  
+  //const userData = route.params.userData;
+  
+  // adicionando mais dados no objeto do cliente
+  //userData["nmUser"] = nmusuario;
+  //userData["descricao"] = descrição;
+  //userData["uri"] = "imagem.png";
 
-  const bttCriarEvento = () => {
-    if (!setnmevento || !descrição || !image || !cep || !numero) {
+  
+  
+  const handleVamosLaPress = () => {
+    if (!nmusuario || !descrição) {
       setErro('Preencha todos os campos obrigatórios.');
       setTimeout(() => {
         setErro('');
       }, 4000);
     } else {
-        setErro('');
-        axios.post('http://localhost:3003/cadEvent', eventData)
-        .then(response => {
-        // Lidar com a resposta do servidor, se necessário
-    
-        console.log(response);
-        navigation.navigate('telaprincipal', { userImage: image });
-    })
-    .catch(error => {
-    // Lidar com erros, se houver algum
-      console.error('Erro ao enviar os dados para o backend:', error);
-    });
+      setErro('');
+      navigation.navigate('termos', { userImage: image, userData: userData });
     }
   };
-
+  // verificando pra ver se ta certo
+  
   return (
     <ImageBackground
-      source={require('./img/telap2.png')}
-      style={styles.container}
-      resizeMode="cover"
+    source={require('./img/telap2.png')}
+    style={styles.container}
+    resizeMode="cover"
     >
+
+      <Pressable style={styles.backButton} onPress={backbutton}>
+        <Image source={require('./img/icons/backicon.png')} style={styles.backIcon} />
+      </Pressable>
 
       <View style={styles.overlay}>
         <View style={styles.content}>
@@ -84,19 +79,19 @@ export default function Cadevento() {
             <Image source={require('./img/icons/Group.png')} style={styles.iconuser} />
             <TextInput
               style={styles.textInput}
-              placeholder="Nome de usuario"
+              placeholder="Nome do evento"
               placeholderTextColor="rgba(255, 255, 255, 0.5)"
               underlineColorAndroid="transparent"
               maxLength={100}
-              value={setnmevento}
-              onChangeText={setNmEvento}
+              value={nmuevento}
+              onChangeText={setNmevento}
             />
           </View>
 
           <View style={styles.textInputContainer}>
             <Image source={require('./img/icons/page.png')} style={styles.icon} />
             <TextInput
-              style={styles.textInput}
+              style={styles.textInput2}
               placeholder="Descrição"
               placeholderTextColor="rgba(255, 255, 255, 0.5)"
               underlineColorAndroid="transparent"
@@ -108,39 +103,9 @@ export default function Cadevento() {
               {renderCaracteresRestantes()}
             </Text>
           </View>
-
-          <View style={styles.textInputContainer}>
-            <Image source={require('./img/icons/location.png')} style={styles.iconlocation} />
-            <TextInput
-              style={styles.textInput}
-              placeholder="CEP"
-              placeholderTextColor="rgba(255, 255, 255, 0.5)"
-              underlineColorAndroid="transparent"
-              maxLength={100}
-              value={cep}
-              onChangeText={setEndereco}
-            />
-          </View>
-
-          <View style={styles.textInputContainer}>
-            <Image source={require('./img/icons/home.png')} style={styles.iconhome} />
-            <TextInput
-              style={styles.textInput}
-              placeholder="Numero"
-              placeholderTextColor="rgba(255, 255, 255, 0.5)"
-              underlineColorAndroid="transparent"
-              maxLength={100}
-              value={numero}
-              onChangeText={setCep}
-            />
-          </View>
         </View>
 
-        <Pressable style={styles.backButton} onPress={backbutton}>
-          <Image source={require('./img/icons/backicon.png')} style={styles.backIcon} />
-        </Pressable>
-
-        <Pressable onPress={handleImagePicker} style={{ top: -455 }}>
+        <Pressable onPress={handleImagePicker} style={{ top: -350 }}>
           <View style={styles.imageContainer}>
             <Image
               source={image ? { uri: image } : require('./img/icons/layer1.png')}
@@ -149,15 +114,20 @@ export default function Cadevento() {
           </View>
         </Pressable>
 
-        <Text style={styles.textadd}>
-            Adicionar Foto
+        <Text style={styles.textTitle}>
+          Adicionar foto
         </Text>
 
-        <Pressable style={styles.button} onPress={bttCriarEvento}>
+
+        <Text style={styles.textTitle2}>
+          Dados do endereço
+        </Text>
+
+
+
+        <Pressable style={styles.button} onPress={handleVamosLaPress}>
           <Text style={styles.buttonText}>Avançar</Text>
         </Pressable>
-
-        
 
         {erro !== '' && (
           <Animatable.View
@@ -176,7 +146,7 @@ export default function Cadevento() {
             <Text style={styles.errorMessage}>{erro}</Text>
           </Animatable.View>
         )}
-    </View>
+      </View>
     </ImageBackground>
   );
 }
@@ -204,45 +174,6 @@ const styles = StyleSheet.create({
     top: 120,
   },
 
-  navbar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: '#380053',
-    padding: 10,
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-
-  navButton: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 10,
-  },
-
-  navButtonImage: {
-    width: 20,
-    height: 20,
-  },
-
-  circleButton: {
-    width: 60,
-    height: 60,
-    backgroundColor: 'transparent',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    left: '50%',
-    marginLeft: -20,
-  },
-
-  circleButtonImage: {
-    width: 70,
-    height: 75,
-  },
-
   button: {
     backgroundColor: 'rgba(255, 1, 108, 0.4)',
     paddingVertical: 14,
@@ -250,7 +181,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
-    top: 80,
+    top: Platform.OS === 'web' ? 140 : 160,
+  },
+
+  backButton: {
+    position: 'absolute',
+    top: Platform.OS === 'web' ? 55 : 50,
+    left: 27,
+    zIndex: 1,
+  },
+
+  backIcon: {
+    width: 30,
+    height: 24,
   },
 
   buttonText: {
@@ -259,23 +202,16 @@ const styles = StyleSheet.create({
     opacity: 0.7,
   },
 
-  backButton: {
-    bottom: 335,
-    right: 144,
-  },
-
   textInputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    maxWidth: Platform.OS === 'web' ? '100%' : '85%',
-    height: Platform.OS === 'web' ? 50 : 55,
+    width: Platform.OS === 'web' ? '140%' : '80%',
+    height: Platform.OS === 'web' ? 55 : 55,
     borderBottomWidth: 1,
     borderBottomColor: '#FFFFFF',
     marginBottom: 13,
     justifyContent: 'center',
-    bottom: -150,
-    position: 'static',
-
+    top: 55,
   },
 
   textInput: {
@@ -284,38 +220,24 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
+  textInput2: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    flex: 1,
+    left: Platform.OS === 'web' ? 33 : 10,
+  },
+
   icon: {
     width: 19,
     height: 24,
-    marginRight: 16,
-    left: 3,
+    marginRight: 14,
+    left: 5,
   },
 
   iconuser: {
     width: 23,
     height: 23,
     marginRight: 14,
-  },
-
-  iconlocation: {
-    width: 20,
-    height: 28,
-    marginRight: 14,
-    opacity: 0.8,
-  },
-
-  iconhome: {
-    width: 22,
-    height: 22,
-    marginRight: 14,
-    opacity: 0.8,
-  },
-
-  textadd: {
-    color: '#FFFFFF',
-    fontSize: 22,
-    bottom: 325,
-    opacity: 0.7,
   },
 
   errorBanner: {
@@ -336,7 +258,7 @@ const styles = StyleSheet.create({
 
   caracteresRestantes: {
     fontSize: 12,
-    marginLeft: 10,
+    marginLeft: 0,
   },
 
   imageContainer: {
@@ -344,11 +266,26 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(255, 255, 255, 0.5)',
     borderRadius: 100,
     overflow: 'hidden',
-    top: 102,
+    top: Platform.OS === 'web' ? 75 : 102,
   },
 
   image: {
     width: 200,
     height: 200,
+  },
+
+  textTitle: {
+    color: '#FFFFFF',
+    fontSize: 22,
+    bottom: 250,
+    opacity: 0.6,
+  },
+
+  textTitle2: {
+    color: '#FFFFFF',
+    fontSize: 22,
+    bottom: 250,
+    opacity: 0.6,
+    top: -35,
   },
 });
