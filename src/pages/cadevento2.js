f08be665271b17ba89c17101b6d9f11c3749b4c1
wesleyandrade013 <wnaa2004@gmail.com>
2023-10-10 13:03:10 -0300

    commit banca

--- src/pages/cadevento2.js
+++ src/pages/cadevento2.js
@@ -364,7 +364,7 @@ export default function Cadevento2() {
           visible={isClassificationTypeMenuVisible}
           onRequestClose={closeClassificationTypeMenu}
         >
-          <Pressable onPress={closeClassificationTypeMenu} style={styles.modalBackground1} >
+          <Pressable onPress={closeClassificationTypeMenu} style={styles.modalBackground} >
             <Animatable.View
               style={styles.menuContainer}
               animation={isClassificationTypeMenuVisible ? 'slideInUp' : 'slideInDown'}
