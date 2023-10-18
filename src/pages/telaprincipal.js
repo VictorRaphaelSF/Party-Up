26f4bb989156a932b66a96237012bd25975df09c
VictorRaphaelSF <ferreir.dsf@gmail.com>
2023-10-18 02:30:17 -0300

    Estado original

--- src/pages/telaprincipal.js
+++ src/pages/telaprincipal.js
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Pressable,
-  Image,
-  Dimensions,
-  Text,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, View, Pressable, Image, Dimensions, Text, ScrollView, FlatList,  } from 'react-native';
 
-import { useRoute, useNavigation } from "@react-navigation/native";
-import axios from "axios";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import { useRoute, useNavigation, useContext } from '@react-navigation/native';
+import axios from 'axios';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 export default function Telaprincipal() {
   const [reload, setReload] = useState(0);
-  const [imgProfile, setImgProfile] = useState("");
   const { params } = useRoute();
   const navigation = useNavigation();
 
@@ -34,7 +25,7 @@ export default function Telaprincipal() {
   const { id } = route.params;
   console.log(id);
   const handleButtonCenter = () => {
-    navigation.navigate("cadevento", { id: id });
+    navigation.navigate('cadevento', {id : id});
   };
 
   const handleButtonNotification = () => {
@@ -42,46 +33,31 @@ export default function Telaprincipal() {
   };
 
   const handleButtonPeople = () => {
-    navigation.navigate("telaprofile", {id : id});
+    navigation.navigate('telaprofile', {id : id})
   };
 
   const handleUserImagePress = () => {
-    console.log("Foto de perfil pressionada");
+    navigation.navigate('telaprofile', {id : id})
   };
 
-  useEffect(() => {
-    
-  })
+
+  
 
   // axios
-  //   .post("http://localhost:3003/viewEvent")
-  //   .then((response) => {
-  //     console.log(response);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Erro ao enviar ou retono de dados para o backend:", error);
-  //   });
+  //     .post('http://localhost:3003/viewEvent')
+  //     .then((response) => {
+  //       console.log(response);
+
+  //     })
+  //     .catch((error) => {
+  //       console.error('Erro ao enviar ou retono de dados para o backend:', error);
+  //     });
 
   const eventosTemporarios = [
-    require("./img/Eventos(Temporarios)/Evento(1).png"),
-    require("./img/Eventos(Temporarios)/Evento(2).png"),
-    require("./img/Eventos(Temporarios)/Evento(3).png"),
+    require('./img/Eventos(Temporarios)/Evento(1).png'),
+    require('./img/Eventos(Temporarios)/Evento(2).png'),
+    require('./img/Eventos(Temporarios)/Evento(3).png'),
   ];
-
-
-  useEffect(() => {
-    axios
-      .post("http://localhost:3003/profileUser", {
-        userName_code: id,
-      })
-      .then((e) => {
-        console.log(e);
-        console.log('====================================');
-        console.log(id);
-        console.log('====================================');
-        setImgProfile(e.data.results[0].Image_data);
-      });
-  }, [])
 
   const renderItem = ({ item }) => (
     <Image style={styles.carouselImage} source={item} />
@@ -90,20 +66,14 @@ export default function Telaprincipal() {
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
-        <Image
-          source={require("./img/icons/sol.png")}
-          style={styles.topIconSol}
-        />
-        <Image
-          source={require("./img/icons/partyuplg.png")}
-          style={styles.topIconPartyup}
-        />
+        <Image source={require('./img/icons/sol.png')} style={styles.topIconSol} />
+        <Image source={require('./img/icons/partyuplg.png')} style={styles.topIconPartyup}/>
       </View>
-
+      
       <View style={styles.topUser}>
         <Pressable onPress={handleUserImagePress}>
           <Image
-            source={`data:image/png;base64,${imgProfile}`}
+            source={params?.userImage ? { uri: params.userImage } : require('./img/icons/people(f).png')}
             style={styles.userImage}
           />
         </Pressable>
@@ -120,75 +90,54 @@ export default function Telaprincipal() {
         contentContainerStyle={styles.carousel}
       />
 
-      <Image
-        source={require("./img/Eventos(Temporarios)/EventoM(1).png")}
+      <Image 
+        source={require('./img/Eventos(Temporarios)/EventoM(1).png')}
         style={styles.backgroundImage}
       />
-      <Image
-        source={require("./img/Eventos(Temporarios)/EventoM(2).png")}
+      <Image 
+        source={require('./img/Eventos(Temporarios)/EventoM(2).png')}
         style={styles.backgroundImage1}
       />
 
       <View style={styles.navbar}>
         <Pressable style={styles.navButton} onPress={handleButtonHome}>
-          <Image
-            source={require("./img/icons/home(g).png")}
-            style={styles.navButtonImage}
-          />
+          <Image source={require('./img/icons/home(g).png')} style={styles.navButtonImage} />
         </Pressable>
 
-        <Pressable
-          style={[styles.navButton, { left: -15 }]}
-          onPress={handleButtonSearch}>
-          <Image
-            source={require("./img/icons/search(g).png")}
-            style={styles.navButtonImage}
-          />
+        <Pressable style={[styles.navButton, { left: -15 }]} onPress={handleButtonSearch}>
+          <Image source={require('./img/icons/search(g).png')} style={styles.navButtonImage} />
         </Pressable>
 
-        <Pressable
-          style={[styles.circleButton, { bottom: 30 }]}
-          onPress={handleButtonCenter}>
-          <Image
-            source={require("./img/icons/add(g).png")}
-            style={styles.circleButtonImage}
-          />
+        <Pressable style={[styles.circleButton, { bottom: 30 }]} onPress={handleButtonCenter}>
+          <Image source={require('./img/icons/add(g).png')} style={styles.circleButtonImage} />
         </Pressable>
 
-        <Pressable
-          style={[styles.navButton, { left: 15 }]}
-          onPress={handleButtonNotification}>
-          <Image
-            source={require("./img/icons/notification(g).png")}
-            style={styles.navButtonImage}
-          />
+        <Pressable style={[styles.navButton, { left: 15 }]} onPress={handleButtonNotification}>
+          <Image source={require('./img/icons/notification(g).png')} style={styles.navButtonImage} />
         </Pressable>
 
         <Pressable style={styles.navButton} onPress={handleButtonPeople}>
-          <Image
-            source={require("./img/icons/people(g).png")}
-            style={styles.navButtonImage}
-          />
+          <Image source={require('./img/icons/people(g).png')} style={styles.navButtonImage} />
         </Pressable>
       </View>
     </View>
   );
 }
 
-const windowHeight = Dimensions.get("window").height;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#260038",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#260038',
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
   },
 
   topBar: {
-    flexDirection: "row",
-    position: "absolute",
+    flexDirection: 'row',
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
@@ -197,16 +146,16 @@ const styles = StyleSheet.create({
   },
 
   topUser: {
-    flexDirection: "row",
-    position: "absolute",
+    flexDirection: 'row',
+    position: 'absolute',
     top: 33,
     right: 22,
-    alignItems: "center",
+    alignItems: 'center',
   },
 
   userImage: {
     width: 45,
-    height: 45,
+    height: 45, 
     borderRadius: 25,
   },
 
@@ -227,23 +176,23 @@ const styles = StyleSheet.create({
   },
 
   highlightsText: {
-    flexDirection: "row",
-    position: "absolute",
+    flexDirection: 'row',
+    position: 'absolute',
     top: 0,
     left: 22,
-    color: "#FFFFFF",
+    color: '#FFFFFF',
     fontSize: 26,
-    fontWeight: "bold",
-    top: 80,
+    fontWeight: 'bold',
+    top: 80,  
     right: 115,
   },
 
   carousel: {
-    alignItems: "center",
+    alignItems: 'center',
     paddingVertical: 24,
     bottom: 170,
     marginVertical: 0,
-    width: "100%",
+    width: '100%',
   },
 
   carouselImage: {
@@ -255,12 +204,12 @@ const styles = StyleSheet.create({
   },
 
   navbar: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "#380053",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#380053',
     padding: 10,
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
@@ -268,7 +217,7 @@ const styles = StyleSheet.create({
 
   navButton: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
     padding: 10,
   },
 
@@ -280,12 +229,12 @@ const styles = StyleSheet.create({
   circleButton: {
     width: 60,
     height: 60,
-    backgroundColor: "transparent",
-    alignItems: "center",
-    justifyContent: "center",
-    position: "absolute",
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
     bottom: 0,
-    left: "50%",
+    left: '50%',
     marginLeft: -27,
   },
 
@@ -295,8 +244,8 @@ const styles = StyleSheet.create({
   },
 
   backgroundImage: {
-    position: "absolute",
-    width: "80%",
+    position: 'absolute',
+    width: '80%',
     bottom: 250,
     height: 200,
     marginTop: 20,
@@ -304,8 +253,8 @@ const styles = StyleSheet.create({
   },
 
   backgroundImage1: {
-    position: "absolute",
-    width: "80%",
+    position: 'absolute',
+    width: '80%',
     bottom: 25,
     height: 200,
     marginTop: 20,
