4e475e4fa826aafc106fb1b184f7433f005b417d
wesleyandrade013 <wnaa2004@gmail.com>
2023-10-22 19:55:29 -0300

    .

--- src/pages/cadastropart2.js
+++ src/pages/cadastropart2.js
@@ -1,94 +1,95 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, Pressable, Image, Platform, Dimensions, TextInput, ImageBackground } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Pressable,
+  Image,
+  Platform,
+  Dimensions,
+  TextInput,
+  ImageBackground,
+} from "react-native";
 
-import * as Animatable from 'react-native-animatable';
-import * as ImagePicker from 'expo-image-picker';
-import { useNavigation } from '@react-navigation/native';
-import axios from 'axios';
+import * as Animatable from "react-native-animatable";
+import * as ImagePicker from "expo-image-picker";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
-export default function Logado({route}) {
-
-  const [nmusuario, setNmusuario] = useState('');
-  const [descrição, setDescrição] = useState('');
-  const [erro, setErro] = useState('');
+export default function Logado({ route }) {
+  const [nmusuario, setNmusuario] = useState("");
+  const [descrição, setDescrição] = useState("");
+  const [erro, setErro] = useState("");
   const [image, setImage] = useState(null);
   const navigation = useNavigation();
-  const [fileName, setFileName] = useState('');
-  
+  const [imageData, setImageData] = useState("");
+
   const handleImagePicker = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       aspect: [4, 4],
       allowsEditing: true,
-      base64: false,
+      base64: true,
       quality: 1,
     });
-  
+
     if (!result.canceled) {
-      const nomeDoArquivo = result.assets[0].uri.split('/').pop();
+      const imageB64 = result.assets[0].base64;
       setImage(result.assets[0].uri);
-      setFileName(nomeDoArquivo);
+      setImageData(imageB64);
     }
-    
+
+    console.log(result.assets[0].base64);
   };
-  
-  
-  
+
   const backbutton = () => {
     navigation.goBack();
   };
-  
+
   const renderCaracteresRestantes = () => {
     const caracteresRestantes = 255 - descrição.length;
-    const corCaracteres = caracteresRestantes === 0 ? '#FF0000' : 'rgba(255, 255, 255, 0.5)';
-    return (
-      <Text style={{ color: corCaracteres }}>
-        {caracteresRestantes}
-      </Text>
-    );
+    const corCaracteres =
+      caracteresRestantes === 0 ? "#FF0000" : "rgba(255, 255, 255, 0.5)";
+    return <Text style={{ color: corCaracteres }}>{caracteresRestantes}</Text>;
   };
-
-  
-  
 
   const userData = route.params.userData;
-  //const userData = route.params.userData;
-  
-  // adicionando mais dados no objeto do cliente
+
+  // // adicionando mais dados no objeto do cliente
   userData["nmUser"] = nmusuario;
   userData["descricao"] = descrição;
-  userData["nmImage"] = fileName;
+  userData["imgData"] = imageData;
 
-  
-  
   const handleVamosLaPress = () => {
     if (!nmusuario || !descrição || !image) {
-      setErro('Preencha todos os campos obrigatórios.');
+      setErro("Preencha todos os campos obrigatórios.");
       setTimeout(() => {
-        setErro('');
+        setErro("");
       }, 4000);
     } else {
-      console.log(fileName)
-      console.log(image)
-      navigation.navigate('termos', { userImage: image, userData: userData });
-    
+      console.log(image);
+      navigation.navigate("termos", { userImage: image, userData: userData });
     }
   };
-  
+
   return (
     <ImageBackground
-    source={require('./img/telap2.png')}
-    style={styles.container}
-    resizeMode="cover"
-    >
-
+      source={require("./img/telap2.png")}
+      style={styles.container}
+      resizeMode="cover">
       <Pressable style={styles.backButton} onPress={backbutton}>
-        <Image source={require('./img/icons/backicon.png')} style={styles.backIcon} />
+        <Image
+          source={require("./img/icons/backicon.png")}
+          style={styles.backIcon}
+        />
       </Pressable>
 
       <View style={styles.overlay}>
         <View style={styles.content}>
           <View style={styles.textInputContainer}>
-            <Image source={require('./img/icons/Group.png')} style={styles.iconuser} />
+            <Image
+              source={require("./img/icons/Group.png")}
+              style={styles.iconuser}
+            />
             <TextInput
               style={styles.textInput}
               placeholder="Nome de usuario"
@@ -101,7 +102,10 @@ export default function Logado({route}) {
           </View>
 
           <View style={styles.textInputContainer}>
-            <Image source={require('./img/icons/page.png')} style={styles.icon} />
+            <Image
+              source={require("./img/icons/page.png")}
+              style={styles.icon}
+            />
             <TextInput
               style={styles.textInput2}
               placeholder="Descrição"
@@ -117,38 +121,36 @@ export default function Logado({route}) {
           </View>
         </View>
 
-
         <Pressable onPress={handleImagePicker} style={{ top: -350 }}>
           <View style={styles.imageContainer}>
             <Image
-              source={image ? { uri: image } : require('./img/icons/layer1.png')}
+              source={
+                image ? { uri: image } : require("./img/icons/layer1.png")
+              }
               style={styles.image}
             />
           </View>
         </Pressable>
 
-        <Text style={styles.textTitle}>
-          Adicionar Foto
-        </Text>
+        <Text style={styles.textTitle}>Adicionar Foto</Text>
 
         <Pressable style={styles.button} onPress={handleVamosLaPress}>
           <Text style={styles.buttonText}>Cadastrar</Text>
         </Pressable>
 
-        {erro !== '' && (
+        {erro !== "" && (
           <Animatable.View
             style={[
               styles.errorBanner,
               {
-                display: erro ? 'flex' : 'none',
+                display: erro ? "flex" : "none",
                 borderRadius: 10,
                 marginTop: erro ? 20 : 0,
               },
             ]}
             animation="shake"
             iterationCount={1}
-            duration={800}
-          >
+            duration={800}>
             <Text style={styles.errorMessage}>{erro}</Text>
           </Animatable.View>
         )}
@@ -157,42 +159,42 @@ export default function Logado({route}) {
   );
 }
 
-const windowHeight = Dimensions.get('window').height;
+const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 
   overlay: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
   },
 
   content: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     top: 120,
   },
 
   button: {
-    backgroundColor: 'rgba(255, 1, 108, 0.4)',
+    backgroundColor: "rgba(255, 1, 108, 0.4)",
     paddingVertical: 14,
     paddingHorizontal: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 10,
-    top: Platform.OS === 'web' ? 140 : 160,
+    top: Platform.OS === "web" ? 140 : 160,
   },
 
   backButton: {
-    position: 'absolute',
-    top: Platform.OS === 'web' ? 55 : 50,
+    position: "absolute",
+    top: Platform.OS === "web" ? 55 : 50,
     left: 27,
     zIndex: 1,
   },
@@ -204,33 +206,33 @@ const styles = StyleSheet.create({
 
   buttonText: {
     fontSize: 18,
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     opacity: 0.7,
   },
 
   textInputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: Platform.OS === 'web' ? '118%' : '85%',
-    height: Platform.OS === 'web' ? 50 : 55,
+    flexDirection: "row",
+    alignItems: "center",
+    width: Platform.OS === "web" ? "118%" : "85%",
+    height: Platform.OS === "web" ? 50 : 55,
     borderBottomWidth: 1,
-    borderBottomColor: '#FFFFFF',
+    borderBottomColor: "#FFFFFF",
     marginBottom: 13,
-    justifyContent: 'center',
+    justifyContent: "center",
     bottom: -150,
   },
 
   textInput: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 16,
     flex: 1,
   },
 
   textInput2: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 16,
     flex: 1,
-    left: Platform.OS === 'web' ? 33 : 10,
+    left: Platform.OS === "web" ? 33 : 10,
   },
 
   icon: {
@@ -247,18 +249,18 @@ const styles = StyleSheet.create({
   },
 
   errorBanner: {
-    backgroundColor: '#FF0000',
+    backgroundColor: "#FF0000",
     padding: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
     top: 12,
     left: 0,
     right: 0,
   },
 
   errorMessage: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 16,
   },
 
@@ -269,10 +271,10 @@ const styles = StyleSheet.create({
 
   imageContainer: {
     borderWidth: 1,
-    borderColor: 'rgba(255, 255, 255, 0.5)',
+    borderColor: "rgba(255, 255, 255, 0.5)",
     borderRadius: 100,
-    overflow: 'hidden',
-    top: Platform.OS === 'web' ? 75 : 102,
+    overflow: "hidden",
+    top: Platform.OS === "web" ? 75 : 102,
   },
 
   image: {
@@ -281,7 +283,7 @@ const styles = StyleSheet.create({
   },
 
   textTitle: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 22,
     bottom: 250,
     opacity: 0.6,
