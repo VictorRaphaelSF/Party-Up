0ec38ea36ab1fc6368a87d2f0dcafbdb156079bd
wesleyandrade013 <wnaa2004@gmail.com>
2023-10-30 15:21:43 -0300

    history feito

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='login'>
+      <Stack.Navigator initialRouteName='index'>
         <Stack.Screen 
           options={{
             headerShown: false
