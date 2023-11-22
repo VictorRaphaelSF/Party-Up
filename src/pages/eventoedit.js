bd968944586d3ee048734995d566fc02ba43f3e5
Victor Ferreira <141746581+VictorRaphaelSF@users.noreply.github.com>
2023-11-22 15:28:43 -0300

    Patch 1.84(Color correction)

--- src/pages/eventoedit.js
+++ src/pages/eventoedit.js
@@ -64,80 +64,80 @@ export default function Eventoedit({ navigation }) {
 
   const [tags, setTags] = useState("");
 
-  // const route = useRoute();
-  // const { id } = route.params;
-  // const { idEvento } = route.params;
-  // const { imgProfile } = route.params;
+  const route = useRoute();
+  const { id } = route.params;
+  const { idEvento } = route.params;
+  const { imgProfile } = route.params;
   // const id = 1;
   // const idEvento = 6;
   // const imgProfile = null;
 
   // console.log(idEvento);
-  // useEffect(() => {
-  //   const idEvent = {
-  //     eventId_code: idEvento,
-  //   };
-  //   axios
-  //     .post("http://localhost:3003/viewEvent", idEvent)
-  //     .then((response) => {
-  //       console.log(response);
-  //       console.log(response.data[0]);
-  //       //nome
-  //       setTitulo(response.data[0].Nm_event);
+  useEffect(() => {
+    const idEvent = {
+      eventId_code: idEvento,
+    };
+    axios
+      .post("http://localhost:3003/viewEvent", idEvent)
+      .then((response) => {
+        console.log(response);
+        console.log(response.data[0]);
+        //nome
+        setTitulo(response.data[0].Nm_event);
 
-  //       // //descrição
-  //       setDescricao(response.data[0].desc_event);
+        // //descrição
+        setDescricao(response.data[0].desc_event);
 
-  //       const dataB = new Date(response.data[0].Dt_begin);
-  //       const anoB = dataB.getFullYear();
-  //       const mesB = String(dataB.getMonth() + 1).padStart(2, "0");
-  //       const diaB = String(dataB.getDate()).padStart(2, "0");
-  //       const horaB = String(dataB.getHours()).padStart(2, "0");
-  //       const minutoB = String(dataB.getMinutes()).padStart(2, "0");
+        const dataB = new Date(response.data[0].Dt_begin);
+        const anoB = dataB.getFullYear();
+        const mesB = String(dataB.getMonth() + 1).padStart(2, "0");
+        const diaB = String(dataB.getDate()).padStart(2, "0");
+        const horaB = String(dataB.getHours()).padStart(2, "0");
+        const minutoB = String(dataB.getMinutes()).padStart(2, "0");
 
-  //       const dtFormatB = diaB + "-" + mesB + "-" + anoB;
-  //       const hrFormatB = horaB + ":" + minutoB;
+        const dtFormatB = diaB + "-" + mesB + "-" + anoB;
+        const hrFormatB = horaB + ":" + minutoB;
 
-  //       setDataInicio(dtFormatB);
-  //       setHoraInicio(hrFormatB);
+        setDataInicio(dtFormatB);
+        setHoraInicio(hrFormatB);
 
-  //       //data fim
-  //       const dataE = new Date(response.data[0].Dt_end);
-  //       const anoE = String(dataE.getFullYear()).padStart(2, "0");
-  //       const mesE = String(dataE.getMonth() + 1).padStart(2, "0");
-  //       const diaE = String(dataE.getDate()).padStart(2, "0");
-  //       const horaE = String(dataE.getHours()).padStart(2, "0");
-  //       const minutoE = String(dataE.getMinutes()).padStart(2, "0");
+        //data fim
+        const dataE = new Date(response.data[0].Dt_end);
+        const anoE = String(dataE.getFullYear()).padStart(2, "0");
+        const mesE = String(dataE.getMonth() + 1).padStart(2, "0");
+        const diaE = String(dataE.getDate()).padStart(2, "0");
+        const horaE = String(dataE.getHours()).padStart(2, "0");
+        const minutoE = String(dataE.getMinutes()).padStart(2, "0");
 
-  //       const dtFormatE = diaE + "-" + mesE + "-" + anoE;
-  //       const hrFormatE = horaE + ":" + minutoE;
+        const dtFormatE = diaE + "-" + mesE + "-" + anoE;
+        const hrFormatE = horaE + ":" + minutoE;
 
-  //       setDataFim(dtFormatE);
-  //       setHoraFim(hrFormatE);
+        setDataFim(dtFormatE);
+        setHoraFim(hrFormatE);
 
-  //       //site
-  //       setSiteInfo(response.data[0].Site_contact);
+        //site
+        setSiteInfo(response.data[0].Site_contact);
 
-  //       setTelefone(response.data[0].Telefone_event);
-  //       setTpEvent(response.data[0].Tp_Event);
-  //       setTpModality(response.data[0].Tp_Modality);
-  //       setCep(response.data[0].cd_cep);
-  //       setComplemento(response.data[0].complemento);
-  //       setIntagram(response.data[0].instagram_user);
-  //       setMoreInfo(response.data[0].more_info);
-  //       setBairro(response.data[0].nm_bairro);
-  //       setCidade(response.data[0].nm_cidade);
-  //       setEstado(response.data[0].nm_estado);
-  //       setRua(response.data[0].nm_rua);
-  //       setNumeroRes(response.data[0].num_residencia);
+        setTelefone(response.data[0].Telefone_event);
+        setTpEvent(response.data[0].Tp_Event);
+        setTpModality(response.data[0].Tp_Modality);
+        setCep(response.data[0].cd_cep);
+        setComplemento(response.data[0].complemento);
+        setIntagram(response.data[0].instagram_user);
+        setMoreInfo(response.data[0].more_info);
+        setBairro(response.data[0].nm_bairro);
+        setCidade(response.data[0].nm_cidade);
+        setEstado(response.data[0].nm_estado);
+        setRua(response.data[0].nm_rua);
+        setNumeroRes(response.data[0].num_residencia);
 
-  //       //tag
-  //       // setTags(response.data[0].Tag_event);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Erro ao enviar os dados para o backend:", error);
-  //     });
-  // }, []);
+        //tag
+        // setTags(response.data[0].Tag_event);
+      })
+      .catch((error) => {
+        console.error("Erro ao enviar os dados para o backend:", error);
+      });
+  }, []);
 
   const handleButtonEdit = () => {
     navigation.navigate("eventoedit2");
@@ -222,7 +222,7 @@ export default function Eventoedit({ navigation }) {
                 <Pressable style={styles.button} onPress={() => setModalVisible(false)}>
                   <Text style={styles.buttonText}>Cancelar</Text>
                 </Pressable>
-                <Pressable style={styles.button} onPress={() => handleConfirmarExclusao()}>
+                <Pressable style={styles.buttonExclusion} onPress={() => handleConfirmarExclusao()}>
                   <Text style={styles.buttonText}>Confirmar</Text>
                 </Pressable>
               </View>
@@ -590,6 +590,14 @@ const styles = StyleSheet.create({
 
   button: {
     backgroundColor: "#7E3CA7",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginHorizontal: 10,
+  },
+
+  buttonExclusion: {
+    backgroundColor: "#b8372e",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 10,
